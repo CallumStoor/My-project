d8139b26bb57399fdbb79edf6feac48af72e83a3
CallumStoor <562395@escg.ac.uk>
2025-10-20 18:08:46 +0100

    20-10-2025

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health Settings")]
     [SerializeField] private int startingHealth;
     public int currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("IFrame")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(int _damage)
@@ -20,6 +29,7 @@ public class Health : MonoBehaviour
         if(currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine((IEnumerator)Invinciblity());
         }
         else
         {
@@ -30,5 +40,18 @@ public class Health : MonoBehaviour
                 dead = true;
             }
         }
+    }
+
+    private IEnumerable Invinciblity()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
